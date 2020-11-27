a1bcc0154f72a379e342f33d1400459d0e36d552
chendongling <785087368@qq.com>
2020-11-27 22:59:45 +0800

    hot-fix commit

--- src/main/java/TestGit.java
+++ src/main/java/TestGit.java
@@ -6,5 +6,6 @@ public class TestGit {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("变色");
+        System.out.println("hot-fix");
     }
 }
