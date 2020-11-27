b48819362fc7d69ffa1e890ddcb6dfc102a29829
chendongling <785087368@qq.com>
2020-11-27 23:16:37 +0800

    push first commit

--- src/main/java/TestGit.java
+++ src/main/java/TestGit.java
@@ -7,5 +7,6 @@ public class TestGit {
         System.out.println("hello world");
         System.out.println("变色");
         System.out.println("hot-fix");
+        System.out.println("push");
     }
 }
